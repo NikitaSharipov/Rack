77970c3592df19c7d46f5433d134c46681b12675
NikitaSharipov <sharipovkzn@mail.ru>
2018-12-01 12:57:35 +0300

    19

--- app.rb
+++ app.rb
@@ -8,18 +8,18 @@ class App
     if request.get? && request.path == "/time"
       time_format(request)
     else
-      output_array(404, "Not Found")
+      make_response(404, "Not Found")
     end
   end
 
   private
 
   def time_format(request)
-    formater = TimeFormat.new(request.params)
+    formater = TimeFormat.new(request.params['format'])
     if formater.valid?
-      output_array(200, formater.valid_output)
+      make_response(200, formater.valid_output)
     else
-      output_array(400, "Unknown time format #{formater.invalid_keys}")
+      make_response(400, "Unknown time format #{formater.invalid_keys}")
     end
   end
 
@@ -27,7 +27,7 @@ class App
     { 'Content-Type' => 'text/plain' }
   end
 
-  def output_array(code, body)
+  def make_response(code, body)
     [code, headers, [body + "\n"]]
   end
 
--- time_format.rb
+++ time_format.rb
@@ -3,7 +3,7 @@ class TimeFormat
   TIME_FORMAT = {'year' => '%Y', 'month' => '%m', 'day' => '%d', 'hour' => '%H', 'minute' => '%M', 'second' => '%S'}
 
   def initialize(formats)
-    @formats = formats['format'].split(',')
+    @formats = formats.split(',')
   end
 
   def valid?
@@ -12,20 +12,20 @@ class TimeFormat
 
   def invalid_keys
     @formats.select do |format|
-      TIME_FORMAT[format].nil?
+      TIME_FORMAT.key?(format)
     end
   end
 
   def valid_keys_value
     time_format = []
-    @formats.each do |format|
+    @formats.map do |format|
       time_format.push(TIME_FORMAT[format])
     end
     time_format
   end
 
   def valid_output
-    Time.now.strftime(valid_keys_value.join('-')).to_s
+    Time.now.strftime(valid_keys_value.join('-'))
   end
 
 end
